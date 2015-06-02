8a9cc71cd28d8c919cb51d203ba8e97e6002a442
Dongsu Park <dpark@posteo.net>
2015-06-02 17:33:23 +0200

    tests: add a test for rename, moveTo, copyTo

--- tests/test_files.py
+++ tests/test_files.py
@@ -88,3 +88,63 @@ def test_upload_string_as_file_content(repo):
     f = rootdir.upload(fcontent, fname)
     assert f.name == fname
     assert f.get_content() == fcontent
+
+'/测试目录一-%s' % randstring()
+@pytest.mark.parametrize('parentpath', [
+    '/',
+    '/seafdir-%s' % randstring()
+])
+def test_rename_move_copy(repo, parentpath):
+    rootdir = repo.get_dir('/')
+    assert len(rootdir.ls()) == 0
+
+    if parentpath == '/':
+        parentdir = rootdir
+    else:
+        parentdir = rootdir.mkdir(parentpath[1:])
+
+    # create a file
+    testfile = parentdir.create_empty_file('测试文件-%s.txt' % randstring())
+    assert testfile.size == 0
+
+    entries = parentdir.ls(force_refresh=True)
+    assert len(entries) == 1
+
+    entry = entries[0]
+    assert entry.path == testfile.path
+    assert entry.id == testfile.id
+    assert entry.size == testfile.size
+
+    # rename a file
+    newfname = 'newfile.txt'
+    testfile.rename(newfname)
+    assert testfile.name == newfname
+
+    # create a folder
+    testdir = parentdir.mkdir('测试目录-%s' % randstring())
+    assert len(parentdir.ls()) == 2
+    assert len(testdir.ls()) == 0
+
+    direntry = [entry for entry in parentdir.ls() if entry.isdir][0]
+    assert direntry.path == testdir.path
+
+    # move a file to the new folder
+    testfile.moveTo(testdir.name, dst_repo=None)
+
+    assert testfile.name == newfname
+    fileentry = [entry for entry in testdir.ls() if not entry.isdir][0]
+    assert fileentry.path == testfile.path
+
+    # copy a file to the parent folder
+    filenames.append(testfile)
+    testfile.copyTo(filenames, parentdir.name, dst_repo=None)
+
+    assert testfile.name == newfname
+    fileentry = [entry for entry in parentdir.ls() if not entry.isdir][0]
+    assert fileentry.path == testfile.path
+
+    # clean up test directories / files
+    testfile.delete()
+    assert len(parentdir.ls(force_refresh=True)) == 1
+    testdir.delete()
+    assert len(parentdir.ls(force_refresh=True)) == 0
