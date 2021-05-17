249b1a3dc154bcc318ac38d266b105b842f731b5
Chris Stenkamp <cstenkamp@gmail.com>
2021-05-17 11:51:17 +0200

    fix upload for file-like non-file objects

--- seafileapi/__init__.py
+++ seafileapi/__init__.py
@@ -1,4 +1,5 @@
 from seafileapi.client import SeafileApiClient
+from setup.py import __version__
 
 def connect(server, username, password):
     client = SeafileApiClient(server, username, password)
