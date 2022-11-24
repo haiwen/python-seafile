8b5807abe06af983d702bc57fb6b26f21aeb1533
Robin Kloppe <git@mainboarder.de>
2022-11-24 18:51:16 +0100

    replaced domain by RFC 2606 domain

--- functests.sh
+++ functests.sh
@@ -3,9 +3,9 @@
 : ${PYTHON=python}
 
 : ${SEAFILE_TEST_SERVER_ADDRESS="http://127.0.0.1:8000"}
-: ${SEAFILE_TEST_USERNAME="test@seafiletest.com"}
+: ${SEAFILE_TEST_USERNAME="test@example.com"}
 : ${SEAFILE_TEST_PASSWORD="testtest"}
-: ${SEAFILE_TEST_ADMIN_USERNAME="admin@seafiletest.com"}
+: ${SEAFILE_TEST_ADMIN_USERNAME="admin@example.com"}
 : ${SEAFILE_TEST_ADMIN_PASSWORD="adminadmin"}
 
 export SEAFILE_TEST_SERVER_ADDRESS
